91c25b97d2c8f46d59396677007f13fda8a4bf18
Ccordova41 <ccordova41@gmail.com>
2019-10-15 21:53:59 -0400

    add clarifai

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import Rank from './components/Rank/Rank'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '1991928e0f9e45ee9340f7c96ffb53eb'
+});
 
 const particleOptions = {
   particles: {
@@ -33,7 +39,19 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
-  }
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b",
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function(response) {
+          console.log(response);
+          // do something with response
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+}
 
   render() {
     return (
@@ -44,9 +62,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange}
-        onButtonSubmit={this.onButtonSubmit}/>
-
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
